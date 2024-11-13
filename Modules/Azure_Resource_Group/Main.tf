5976e2e16dbc773ce322460e16766bc65006bb12
prashant007 <prashantgi1995@gmail.com>
2024-11-13 16:14:16 +0530

    added stA

--- Modules/Azure_Resource_Group/Main.tf
+++ Modules/Azure_Resource_Group/Main.tf
@@ -3,7 +3,10 @@ resource "azurerm_resource_group" "RG" {
   location = "westus"
 }
 
-resource "azurerm_resource_group" "RG1" {
-  name = "${prefix}-rg"
-  location = "canadacentral"
+resource "azurerm_storage_account" "sa" {
+  name                     = "sa1"
+  location                 = "westus"
+  account_replication_type = "GRS"
+  account_tier             = "Standard"
+  resource_group_name      = "rg"
 }
