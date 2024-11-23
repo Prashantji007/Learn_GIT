de1890f7e2750ea6a7f517dd82b43d7e6af0125c
prashant007 <prashantgi1995@gmail.com>
2024-11-23 22:50:40 +0530

    Check branch Rule

--- Environments/Dev/Main.tf
+++ Environments/Dev/Main.tf
@@ -1,3 +1,39 @@
 module "rg" {
   source = "../../Modules/Azure_Resource_Group"
 }
+
+locals {
+  location = "delhi"
+  Rule = "Pushpa 2"
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "veerv"
+  location            = "centralindia"
+  resource_group_name = "${var.prefix}-rg"
+  size                = "Standard_B2s"
+  admin_username      = "admin"
+  admin_password      = "Test@123"
+  disable_password_authentication = false
+  network_interface_ids = ""
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  tags = "Dev"
+}
+resource "azurerm_storage_account" "sa" {
+  name                     = "sa1"
+  location                 = "westus"
+  account_replication_type = "GRS"
+  account_tier             = "Standard"
+  resource_group_name      = "rg"
+}
