a084b11140e79cbeeb50e3b69464b92d7a57b6f2
prashant007 <prashantgi1995@gmail.com>
2024-12-02 22:59:43 +0530

    Added resource group for canadaeast location as per the requirement

--- Modules/Azure_Resource_Group/Main.tf
+++ Modules/Azure_Resource_Group/Main.tf
@@ -14,6 +14,12 @@ resource "azurerm_resource_group" "SRG" {
 }
 
 
+
+resource "azurerm_resource_group" "RG" {
+  name     = "${var.prefix}-prashant-rg"
+  location = "canadaeast"
+}
+
 resource "azurerm_virtual_network" "vn" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
